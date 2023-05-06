ce6514d597047ac68b67107735dc7fada6040585
Pipexlul <felipgg@gmail.com>
2023-05-06 18:06:26 -0400

    Apply validateSong middleware to post route

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import cors from "cors";
 
 import songsRoutes from "./routes/songs.js";
 
+import validateMW from "./middleware/validateSong.js";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/canciones", songsRoutes.getSongs);
-app.post("/canciones", songsRoutes.createSong);
+app.post("/canciones", validateMW, songsRoutes.createSong);
 app.put("/canciones/:id", songsRoutes.updateSong);
 app.delete("/canciones/:id", songsRoutes.deleteSong);
